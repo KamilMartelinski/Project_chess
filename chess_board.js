2f44bca79714078ccb9756cce1ef49a93d2f8380
Kmil Mrtelini <kamilmartelinskii@gmail.com>
2024-07-08 14:14:56 +0200

    added function that generates chess board

--- chess_board.js
+++ chess_board.js
@@ -1,7 +1,33 @@
-function createChessBoard(spaceForBoardId){
+function createPlayableChessBoard(){
 
-    const spaceForBoard = document.getElementById(spaceForBoardId);
+    createChessBoard();
+}  
 
-    const chessBoard = document.createElement("table");
-    const row1 = document.createElement("tr");
+function createChessBoard(){
+
+    let rowNumber = 1;
+     
+    for(rowNumber; rowNumber <= 8; rowNumber++){
+
+        let columnNumber = 1;
+
+        for(columnNumber; columnNumber <= 8; columnNumber++){
+
+            let columnCharacter = `a`;
+            
+            for(let i = 1; i <= columnNumber - 1; i++){
+                const nextCharacter = character => String.fromCharCode(character.charCodeAt(0) + 1);
+                columnCharacter = nextCharacter(columnCharacter);
+            }
+
+            console.log(columnCharacter);
+            console.log(rowNumber);
+        }
+    }
 }
+
+function addPaces(){
+
+}
+
+createPlayableChessBoard();
