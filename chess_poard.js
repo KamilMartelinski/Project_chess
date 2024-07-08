c9f2b4c335fec6b508f29c2da0fcc008dcf2e119
Kmil Mrtelini <kamilmartelinskii@gmail.com>
2024-07-08 08:33:01 +0200

    working on createChessBoard function

---
+++ chess_poard.js
@@ -0,0 +1,7 @@
+function createChessBoard(spaceForBoardId){
+
+    const spaceForBoard = document.getElementById(spaceForBoardId);
+
+    const chessBoard = document.createElement("table");
+    const row1 = document.createElement("tr");
+}
